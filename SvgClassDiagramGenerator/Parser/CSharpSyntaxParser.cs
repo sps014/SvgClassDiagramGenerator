9a22306c389b376a0b01a7523ed63302f4be8f19
Shivendra Pratap Singh <shivendrapsingh014@gmail.com>
2024-05-15 20:36:24 +0530

    Initial commit

---
+++ SvgClassDiagramGenerator/Parser/CSharpSyntaxParser.cs
@@ -0,0 +1,169 @@
+﻿using System.Text;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace SvgClassDiagramGenerator.Parser;
+
+public class CSharpSyntaxParser
+{
+    private SyntaxTree SyntaxTree { get; init; }
+    private SyntaxNode? _root;
+    private SyntaxNode Root
+    {
+        get
+        {
+            if (_root == null)
+                _root = SyntaxTree.GetCompilationUnitRoot();
+            return _root;
+        }
+    }
+
+    public CSharpSyntaxParser(string text)
+    {
+        SyntaxTree = CSharpSyntaxTree.ParseText(text);
+    }
+
+    public ParsedSyntaxInfo Parse()
+    {
+        return new ParsedSyntaxInfo()
+        {
+            Classes = GetAllClasses()
+        };
+    }
+
+    private List<ClassSyntax> GetAllClasses()
+    {
+        var classes = Root.DescendantNodes().OfType<ClassDeclarationSyntax>();
+        List<ClassSyntax> classSyntaxes = new();
+
+        foreach (var classDeclarationSyntax in classes)
+        {
+            classSyntaxes.Add(ParseClass(classDeclarationSyntax));
+        }
+
+        return classSyntaxes;
+    }
+
+    private ClassSyntax ParseClass(ClassDeclarationSyntax @class)
+    {
+        return new ClassSyntax()
+        {
+            Name = GetClassName(@class),
+            BaseClasses = GetBaseClass(@class),
+            Functions = GetFunctions(@class),
+            Properties = GetProperties(@class)
+        };
+    }
+
+    private List<DataMember> GetProperties(ClassDeclarationSyntax @class)
+    {
+        List<DataMember> properties = new List<DataMember>();
+
+        foreach (var prop in @class.DescendantNodes().OfType<PropertyDeclarationSyntax>())
+        {
+            properties.Add(GetPropertyMember(prop));
+        }
+
+        foreach (var f in @class.DescendantNodes().OfType<FieldDeclarationSyntax>())
+        {
+            properties.AddRange(GetFieldMembers(f));
+        }
+
+        return properties;
+    }
+
+    private DataMember GetPropertyMember(PropertyDeclarationSyntax prop)
+    {
+        return new DataMember()
+        {
+            IsPublic = IsPublic(prop.Modifiers),
+            Name = prop.Identifier.ValueText,
+            Type = prop.Type.ToString(),
+        };
+    }
+
+    private List<DataMember> GetFieldMembers(FieldDeclarationSyntax field)
+    {
+        List<DataMember> dataMembers = new List<DataMember>();
+        foreach (var f in field.Declaration.Variables)
+        {
+            dataMembers.Add(new DataMember()
+            {
+                IsPublic = IsPublic(field.Modifiers),
+                Name = f.Identifier.Text,
+                Type = field.Declaration.Type.ToString(),
+            });
+        }
+        return dataMembers;
+    }
+
+    private List<MemberFunction> GetFunctions(ClassDeclarationSyntax @class)
+    {
+        List<MemberFunction> functions = new();
+
+        foreach (var func in @class.DescendantNodes().OfType<MethodDeclarationSyntax>())
+        {
+            functions.Add(GetFunction(func));
+        }
+        return functions;
+    }
+
+    private MemberFunction GetFunction(MethodDeclarationSyntax func)
+    {
+        return new MemberFunction()
+        {
+            IsPublic = IsPublic(func.Modifiers),
+            Name = func.Identifier.Text,
+            Type = func.ReturnType.ToString(),
+            Arguments = GetArguments(func)
+        };
+    }
+
+    private List<Parameter> GetArguments(MethodDeclarationSyntax func)
+    {
+        List<Parameter> arguments = new();
+
+        if (func.ParameterList == null)
+            return arguments;
+
+        foreach (var arg in func.ParameterList.DescendantNodes().OfType<ParameterSyntax>())
+        {
+            arguments.Add(new Parameter()
+            {
+                Name = arg.Identifier.Text,
+                Type = arg.Type!.ToString()
+            });
+        }
+
+        return arguments;
+    }
+
+    private bool IsPublic(SyntaxTokenList token)
+    {
+        return token.Any(x => x.IsKind(SyntaxKind.PublicKeyword));
+    }
+
+    private string? GetBaseClass(ClassDeclarationSyntax @class)
+    {
+        if (@class.BaseList == null)
+            return string.Empty;
+
+        StringBuilder stringBuilder = new StringBuilder(string.Empty);
+
+        var baseNodes = @class.BaseList.DescendantNodes().OfType<BaseTypeSyntax>().ToList();
+        for (int i = 0; i < baseNodes.Count; i++)
+        {
+            stringBuilder.Append(baseNodes[i].ToString());
+
+            if (i != baseNodes.Count - 1)
+                stringBuilder.Append(", ");
+        }
+        return stringBuilder.ToString();
+    }
+
+    private string GetClassName(ClassDeclarationSyntax @class)
+    {
+        return @class.Identifier.ValueText;
+    }
+}
---
+++ SvgClassDiagramGenerator/Parser/ParsedSyntaxInfo.cs
@@ -0,0 +1,65 @@
+﻿
+namespace SvgClassDiagramGenerator.Parser;
+
+public class ParsedSyntaxInfo
+{
+    public required List<ClassSyntax> Classes { get; init; } = new List<ClassSyntax>();
+}
+
+public class ClassSyntax
+{
+    public required string Name { get; init; }
+
+    public string? BaseClasses { get; init; }
+
+    public List<DataMember> Properties { get; init; } = new();
+    public List<MemberFunction> Functions { get; init; } = new();
+
+}
+
+public abstract record Member
+{
+    /// <summary>
+    /// Is modifier public
+    /// </summary>
+    public required bool IsPublic { get; init; }
+    /// <summary>
+    /// Type of property or return type of function
+    /// </summary>
+    public required string Type { get; init; }
+    /// <summary>
+    /// Name if the Property or field or method
+    /// </summary>
+    public required string Name { get; init; }
+
+    /// <summary>
+    /// The Class that can be linked to it to draw conenction line
+    /// </summary>
+    public ClassSyntax? LinksClass { get; init; }
+}
+
+public record MemberFunction : Member
+{
+    public List<Parameter> Arguments { get; init; } = new List<Parameter>();
+}
+
+public record DataMember : Member;
+
+public record AttributeInfo(bool IsPublic);
+
+public record Parameter
+{
+    /// <summary>
+    /// Type of property or return type of function
+    /// </summary>
+    public required string Type { get; init; }
+    /// <summary>
+    /// Name if the Property or field or method
+    /// </summary>
+    public required string Name { get; init; }
+
+    /// <summary>
+    /// The Class that can be linked to it to draw conenction line
+    /// </summary>
+    public ClassSyntax? LinksClass { get; init; }
+}
